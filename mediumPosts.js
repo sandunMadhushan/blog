b3856574521ed8378a3a5ea76b2914eab1cf74e0
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2025-03-25 00:37:15 +0530

    add default-thumbnail

--- mediumPosts.js
+++ mediumPosts.js
@@ -16,7 +16,7 @@ async function displayMediumPosts() {
         if (!thumbnail || thumbnail === "") {
           const imgRegex = /<img[^>]+src="([^">]+)"/;
           const match = post.content.match(imgRegex);
-          thumbnail = match ? match[1] : "default-thumbnail.jpeg";
+          thumbnail = match ? match[1] : "default-thumbnail.png";
         }
 
         // Create short description (remove HTML tags and limit length)
