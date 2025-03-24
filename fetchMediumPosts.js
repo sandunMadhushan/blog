1f6d4ff110fb88cbbbd8bae4abd6801a0cf73981
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2025-03-24 20:17:16 +0530

    Integrated Medium RSS feed

---
+++ fetchMediumPosts.js
@@ -0,0 +1,20 @@
+const { parse } = require("rss-to-json");
+
+async function fetchMediumPosts(username) {
+  try {
+    const rssUrl = `https://medium.com/feed/@${username}`;
+    const rssData = await parse(rssUrl);
+    const posts = rssData.items.map((post) => ({
+      title: post.title,
+      link: post.link,
+      pubDate: post.published,
+      thumbnail: post.enclosure?.url || "", // Optional thumbnail
+    }));
+    console.log(posts);
+    return posts;
+  } catch (error) {
+    console.error("Error fetching Medium posts:", error);
+  }
+}
+
+fetchMediumPosts("sandunmadhushan");
